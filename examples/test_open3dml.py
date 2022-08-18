1de1e7213b4f2440a1995f35498b2e6aa72e8a59
Levin Jian <jianzhirong@gmail.com>
2022-08-18 15:11:39 +0800

    add test open3d ml script

---
+++ examples/test_open3dml.py
@@ -0,0 +1,23 @@
+# import open3d.ml.torch as ml3d  # or open3d.ml.tf as ml3d
+from ml3d.datasets import NuScenes, SemanticKITTI
+from  ml3d.vis import Visualizer
+
+
+
+# construct a dataset by specifying dataset_path
+# dataset = NuScenes(dataset_path='/home/levin/workspace/data/temp/nuscenes/v1.0-mini')
+
+dataset = SemanticKITTI(dataset_path='/home/levin/workspace/data/temp/semantic_kiti/SemanticKitti')
+
+# get the 'all' split that combines training, validation and test set
+all_split = dataset.get_split('train')
+
+# print the attributes of the first datum
+print(all_split.get_attr(0))
+
+# print the shape of the first point cloud
+print(all_split.get_data(0)['point'].shape)
+
+# show the first 100 frames using the visualizer
+vis = Visualizer()
+vis.visualize_dataset(dataset, 'train', indices=range(10))
